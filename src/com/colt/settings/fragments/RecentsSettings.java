3dae6169de143662c083bde0c8537cdf35a21bae
Shubham Singh <coolsks94@gmail.com>
2018-10-29 19:33:06 +0530

    Allow users to switch between Pie/Oreo recents [2/3]

--- src/com/colt/settings/fragments/RecentsSettings.java
+++ src/com/colt/settings/fragments/RecentsSettings.java
@@ -35,13 +35,17 @@ import com.android.settings.R;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.internal.util.colt.ColtUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class RecentsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+
     private ListPreference mRecentsClearAllLocation;
+    private ListPreference mRecentsComponentType;
     private SwitchPreference mRecentsClearAll;
 
     @Override
@@ -60,6 +64,13 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -70,9 +81,21 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
-        return true;
+            return true;
+        } else if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            ColtUtils.showSystemUiRestartDialog(getContext());
+            return true;
         }
-    return false;
+      return false;
 
     }
 
