86ccd5152819ce5f665c9909242489d39f89b082
RakeshBatra <raakesh.batra@rediffmail.com>
2019-01-05 03:03:42 +0000

    Update About Team
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/About.java
+++ src/com/colt/settings/fragments/About.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2017 ColtOS
+ * Copyright (C) 2017-18 ColtOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,71 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.colt.settings.fragments;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class About extends SettingsPreferenceFragment {
-
-    public static final String TAG = "About";
-
-    private String KEY_COLT_SOURCE = "colt_source";
-    private String KEY_COLT_GPLUS = "colt_google_plus";
-    private String KEY_COLT_SHARE = "colt_share";
-
-    private Preference mSourceUrl;
-    private Preference mGoogleUrl;
-    private Preference mShare;
-
+public class About extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     @Override
-	public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.colt_about);
-
-        mSourceUrl = findPreference(KEY_COLT_SOURCE);
-        mGoogleUrl = findPreference(KEY_COLT_GPLUS);
-        mShare = findPreference(KEY_COLT_SHARE);
-
-   }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mSourceUrl) {
-            launchUrl("https://github.com/colt-enigma");
-        } else if (preference == mGoogleUrl) {
-            launchUrl("https://plus.google.com/u/2/communities/102067248475500399675");
-	} else if (preference == mShare) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, String.format(
-                    getActivity().getString(R.string.share_message), Build.MODEL));
-            startActivity(Intent.createChooser(intent, getActivity().getString(R.string.share_chooser_title)));
-        }
-        return super.onPreferenceTreeClick(preference);
+        addPreferencesFromResource(R.xml.about_coltos);
     }
 
-    private void launchUrl(String url) {
-        Uri uriUrl = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
-        getActivity().startActivity(intent);
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.COLT;
     }
-
 }
