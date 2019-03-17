1c3122c71a27180d4e9775904766cb9c5eda3c0d
RakeshBatra <raakesh.batra@rediffmail.com>
2019-03-17 01:10:02 +0000

    Change default clock font

--- src/com/colt/settings/fragments/LockScreenUi.java
+++ src/com/colt/settings/fragments/LockScreenUi.java
@@ -67,14 +67,14 @@ public class LockScreenUi extends SettingsPreferenceFragment implements
         // Lockscren Clock Fonts
         mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
         mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 29)));
+                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 28)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
 
         // Lockscren Date Fonts
         mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
         mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 29)));
+                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 28)));
         mLockDateFonts.setSummary(mLockDateFonts.getEntry());
         mLockDateFonts.setOnPreferenceChangeListener(this);
 
