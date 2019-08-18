AospExtended/platform_packages_apps_Extensions

Gaming mode revamped [2/4]
Change-Id: Id00718f6fb3c73991d5127cd5e67720455b86dde
Signed-off-by: PMS22 <prathams99@rediff.com>
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

src/org/aospextended/extensions/fragments/DynamicMode.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2019 FireHound
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aospextended.extensions.fragments;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.R;
+
+public class DynamicMode extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.dynamic_mode);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.dynamic_mode_footer);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSIONS;
+    }
+}

Colt-Enigma/platform_packages_apps_ColtCenter

src/com/colt/settings/fragments/DynamicMode.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2019 FireHound
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.colt.settings.fragments;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.settings.R;
+
+public class DynamicMode extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.dynamic_mode);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.dynamic_mode_footer);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.COLT;
+    }
+}